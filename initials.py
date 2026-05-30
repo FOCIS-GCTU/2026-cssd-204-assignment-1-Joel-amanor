733b3382cb9c579a4f33f14b5466aa0fa9be5a1e
Joel-amanor <joelamanor46@gmail.com>
2026-05-30 22:33:11 +0000

    Add function to print initials in stylized letters
    Implemented a function to print initials in a stylized format.

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Joel Eric Amanor
+# STUDENT ID: 2425400626
+# Email: 2425400626@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Joel Eric Amanor, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters.
+    print()
+    print()
+    print("...JEA")
+    print()
 
+    print("...JJJJJJJJJJJJ........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...JJJJJJJJJJJJ........EEEEEEEEEEEE........AAAAAAAAAAAA.....")
+    print("...........JJJJ........EEEEE...............AAAA....AAAA.....")
+    print("...........JJJJ........EEEEE...............AAAA....AAAA.....")
+    print("...........JJJJ........EEEEEEEEEEE.........AAAAAAAAAAAA.....")
+    print("...........JJJJ........EEEEEEEEEEE.........AAAAAAAAAAAA.....")
+    print("...........JJJJ........EEEEE...............AAAA....AAAA.....")
+    print("...JJJJ...JJJJ.........EEEEE...............AAAA....AAAA.....")
+    print("....JJJJ..JJJJ....**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print(".....JJJJJJJJ.....**...EEEEEEEEEEEE...**...AAAA....AAAA...**")
+    print()
+    print()
 
 
 main()
